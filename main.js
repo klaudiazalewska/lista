e867da6991ee5d866ea354b58076ce47ada04b9b
klaudiazalewska <kaza2809@gmail.com>
2022-02-04 19:25:42 +0100

    zrobione

--- main.js
+++ main.js
@@ -3,15 +3,45 @@
 window.onload = function () {
     fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json()).then(users => {
     let ul = document.createElement('ul');
-    ul.style.justifyContent = "spaceBetween";
     document.body.appendChild(ul);
 
     for (let i = 0; i < users.length; i++) {
               const user = users[i];
-              const li = document.createElement('li');
-              li.classList.add("elements");
-              li.textContent = ` ${user.name} ${user.email}`;
-              document.querySelector('ul').appendChild(li);
+              const liNames = document.createElement('li');
+              liNames.style.listStyle = "none";
+              liNames.style.marginBottom = "65px";
+              liNames.style.fontSize = "12px";
+              liNames.style.fontWeight = "bold";
+              liNames.textContent = `${user.name}`;
+              document.querySelector('.names ul').appendChild(liNames);
+              const liUsernames = document.createElement('li');
+              liUsernames.style.listStyle = "none";
+              liUsernames.style.marginBottom = "65px";
+              liUsernames.style.fontSize = "12px";
+              liUsernames.style.fontWeight = "bold";
+              liUsernames.textContent = `${user.username}`;
+              document.querySelector('.usernames ul').appendChild(liUsernames);
+              const liEmails = document.createElement('li');
+              liEmails.style.listStyle = "none";
+              liEmails.style.marginBottom = "65px";
+              liEmails.style.fontSize = "12px";
+              liEmails.style.fontWeight = "bold";
+              liEmails.textContent = `${user.email}`;
+              document.querySelector('.emails ul').appendChild(liEmails);
+              const liPhones = document.createElement('li');
+              liPhones.style.listStyle = "none";
+              liPhones.style.marginBottom = "65px";
+              liPhones.style.fontSize = "12px";
+              liPhones.style.fontWeight = "bold";
+              liPhones.textContent = `${user.phone}`;
+              document.querySelector('.phones ul').appendChild(liPhones);
+              const liWebsites = document.createElement('li');
+              liWebsites.style.listStyle = "none";
+              liWebsites.style.marginBottom = "65px";
+              liWebsites.style.fontSize = "12px";
+              liWebsites.style.fontWeight = "bold";
+              liWebsites.textContent = `${user.website}`;
+              document.querySelector('.websites ul').appendChild(liWebsites);
           }
   
         });
